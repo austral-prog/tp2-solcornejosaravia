324d451c2ffb5a52b7671ce51ed2268724f42760
solcornejosaravia <scornejosaravia@mail.austral.edu.ar>
2025-08-18 12:57:28 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,11 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name= f'{first_name} {last_name}'
+    print(full_name.lower())
+    print(full_name.title())
+    print(full_name.upper())
+
+    sangria=f'\t{full_name}'
+    print(sangria.lower())
+ada()
